44dff9436c4e40fd647d361e3481327beb930013
Dustin Meijer <du.a.meijer@googlemail.com>
2015-12-17 13:51:14 +0100

    Created a Strategy class to be usder by the SimulatedAnnealingOptimizer

---
+++ GOO/Model/Optimizers/SimulatedAnnealing/Strategy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOO.Model.Optimizers.SimulatedAnnealing
+{
+    public abstract class Strategy
+    {
+        public Strategy()
+        {
+
+        }
+
+        public virtual Solution executeStrategy(Solution toStartFrom);
+    }
+}
