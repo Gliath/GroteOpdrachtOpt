332cd4f741071a9cd23c6c601ea7ecdc891345ac
Gliath <lukebuitendijk@gmail.com>
2015-12-21 15:30:04 +0100

    Made Strategy abstract

--- GOO/Model/Optimizers/Strategies/Strategy.cs
+++ GOO/Model/Optimizers/Strategies/Strategy.cs
@@ -2,17 +2,9 @@
 
 namespace GOO.Model.Optimizers.Strategies
 {
-    public class Strategy
+    public abstract class Strategy
     {
-        // TODO : Figure out how to undo strategies
-        public virtual Solution executeStrategy(Solution toStartFrom)
-        {
-            return toStartFrom;
-        }
-
-        public virtual Solution undoStrategy(Solution toStartFrom)
-        {
-            return toStartFrom;
-        }
+        public abstract Solution executeStrategy(Solution toStartFrom);
+        public abstract Solution undoStrategy(Solution toStartFrom);
     }
 }
