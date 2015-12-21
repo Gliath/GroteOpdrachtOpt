a15097232c971124d109f6db51265856d642e3d2
Gliath <lukebuitendijk@gmail.com>
2015-12-21 15:25:54 +0100

    Changed Clusters to be a member of AbstractCluster Added MarriedCluster as member of AbstractCluster to 'unite' two clusters

---
+++ GOO/Model/MarriedCluster.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+using GOO.Utilities;
+
+namespace GOO.Model
+{
+    public class MarriedCluster : AbstractCluster
+    {
+        public Cluster Groom { get; set; }
+        public Cluster Bride { get; set; }
+
+        public override List<Order> OrdersInCluster
+        {
+            get
+            {
+                return null;
+            }
+
+            set { return; }
+        }
+
+        public override List<Days> DaysRestrictions
+        {
+            get
+            {
+                return null;
+            }
+
+            set { return; }
+        }
+
+        public override Days DaysPlannedFor
+        {
+            get
+            {
+                return Days.None;
+            }
+
+            set { return; }
+        }
+
+        public override OrdersCounter OrdersCounter
+        {
+            get
+            {
+                return null;
+            }
+
+            set { return; }
+        }
+
+        public MarriedCluster(Cluster Groom, Cluster Bride)
+        {
+            this.Groom = Groom;
+            this.Bride = Bride;
+        }
+    }
+}
