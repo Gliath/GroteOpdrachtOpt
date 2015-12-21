ddfb8c78e804fbbd5b5783b0be6042439a8d2ebe
Gliath <lukebuitendijk@gmail.com>
2015-12-21 17:26:06 +0100

    Made foundation for implementing the Window

--- GOO/ViewModel/MainViewModel.cs
+++ GOO/ViewModel/MainViewModel.cs
@@ -4,6 +4,38 @@ namespace GOO.ViewModel
 {
     public class MainViewModel : INPC
     {
+        public MainViewModel()
+        {
+            ProgressMaximum = 1;
+            ProgressValue = 0;
+        }
 
+        private double progressMaximum;
+        public double ProgressMaximum
+        {
+            get { return progressMaximum; }
+            set
+            {
+                if (value < 1)
+                    value = 1;
+
+                progressMaximum = value;
+                OnPropertyChanged("ProgressMaximum");
+            }
+        }
+
+        private double progressValue;
+        public double ProgressValue
+        {
+            get { return progressValue; }
+            set
+            {
+                if (value < 0)
+                    value = 0;
+
+                progressValue = value;
+                OnPropertyChanged("ProgressValue");
+            }
+        }
     }
 }
