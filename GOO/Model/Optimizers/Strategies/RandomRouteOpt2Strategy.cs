3110a2464fad991721b6d760d4bd1fce247cc1d2
Dustin Meijer <du.a.meijer@googlemail.com>
2015-12-21 21:52:41 +0100

    WIP RandomRouteOpt2Strategy optimization.

--- GOO/Model/Optimizers/Strategies/RandomRouteOpt2Strategy.cs
+++ GOO/Model/Optimizers/Strategies/RandomRouteOpt2Strategy.cs
@@ -13,55 +13,74 @@ namespace GOO.Model.Optimizers.Strategies
         private Days day;
         private int truck;
         private Route old_route;
-        private Route route;
+        private Route new_route;
+        private Route routeToWorkWith;
+        private List<Route> RoutesFromSolution;
 
         public override Solution executeStrategy(Solution toStartFrom)
         {
-            Solution toReturn = toStartFrom;
-
             Tuple<Days, int, List<Route>> Planning = toStartFrom.getRandomPlanningForATruck();
             day = Planning.Item1;
             truck = Planning.Item2;
-            List<Route> Routes = Planning.Item3;
-            route = Routes[new Random().Next(Routes.Count)];
+            RoutesFromSolution = Planning.Item3;
+            
+            //save the begin route for rollback
+            old_route = RoutesFromSolution[new Random().Next(RoutesFromSolution.Count)];
 
-            //copy the begin route for rollback
-            old_route = new Route(day);
-            foreach(Order order in route.Orders)
+            //copy the begin route for the 2-opt
+            routeToWorkWith = new Route(day);
+            foreach(Order order in old_route.Orders)
             {
-                old_route.AddOrder(order); 
+                routeToWorkWith.AddOrder(order); 
             }
 
             //create the route for the 2-opt check
-            Route new_route = new Route(day);
-            foreach (Order order in route.Orders)
+            new_route = new Route(day);
+            foreach (Order order in old_route.Orders)
             {
                 new_route.AddOrder(order);
             }
 
             //start doing the opt-2 algorithm on the route list
+            double best_traveltime = old_route.TravelTime;
+            double new_traveltime = double.MaxValue;
+
             int improvestep = 0;
-            while( improvestep < 3)
+            while( improvestep < 30)
             {
-                double best_traveltime = route.TravelTime;
-                for ( int i = 0; i < route.Orders.Count-1; i ++)
+                for ( int i = 0; i < routeToWorkWith.Orders.Count-1; i ++)
                 {
-                    for (int k = i + 1; k < route.Orders.Count-1; k++)
+                    for (int k = i + 1; k < routeToWorkWith.Orders.Count - 1; k++)
                     {
                         //swap the 2 coords
-                        swapOrders(route.Orders[i], route.Orders[k], new_route);
-                        double new_traveltime = new_route.TravelTime;
+                        swapOrders(routeToWorkWith.Orders[i], routeToWorkWith.Orders[k], new_route);
+                        new_traveltime = routeToWorkWith.TravelTime;
                         if(new_traveltime < best_traveltime)
                         {
                             improvestep = 0;
-                            route = new_route;
+                            new_route = routeToWorkWith;
                             best_traveltime = new_traveltime;
+                            routeToWorkWith = new Route(day);
+                            foreach (Order order in new_route.Orders)
+                            {
+                                routeToWorkWith.AddOrder(order);
+                            }
                         }
                     }
                 }
                 improvestep++;
             }
-            return toReturn;
+
+            Console.WriteLine("Old Travel Time : {0}", old_route.TravelTime);
+            Console.WriteLine("Best Travel Time : {0}", best_traveltime);
+
+            RoutesFromSolution.Remove(old_route);
+            RoutesFromSolution.Add(new_route);
+
+            toStartFrom.RemoveItemFromPlanning(day, truck);
+            toStartFrom.AddNewItemToPlanning(day, truck, RoutesFromSolution);
+
+            return toStartFrom;
         }
 
         private void swapOrders(Order A, Order B, Route route)
@@ -72,9 +91,14 @@ namespace GOO.Model.Optimizers.Strategies
 
         public override Solution undoStrategy(Solution toStartFrom)
         {
-            Solution toReturn = toStartFrom;
-            route = old_route; // should update the corresponding refferences to the created list
-            return toReturn;
+            // Nette manier zou zijn om de tuple weer op te halen gebaseerd op truck en dag.
+            RoutesFromSolution.Remove(new_route);
+            RoutesFromSolution.Add(old_route);
+
+            toStartFrom.RemoveItemFromPlanning(day, truck);
+            toStartFrom.AddNewItemToPlanning(day, truck, RoutesFromSolution);
+
+            return toStartFrom;
         }
     }
 }
