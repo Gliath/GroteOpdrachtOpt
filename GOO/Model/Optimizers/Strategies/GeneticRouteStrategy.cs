b78d51cb0fc01fa59fa62c329c1c1e3133488de8
Gliath <lukebuitendijk@gmail.com>
2015-12-22 13:12:13 +0100

    Added a method in Solution to get a certain planning with a given route
    Finished first iteration of GeneticRouteStrategy.
    In this Strategy a random route will be selected in a random cluster.
    The specified cluster must have enough routes ( > 0) and the route in the target cluster must have enough orders ( > 2, 1 is the 0 order so do not count that one and it need at least to elements to swap), otherwise a different route/cluster will be selected.
    Then it will randomly calculate the number of slices it is going to make and where to make them.
    When this is done it will randomly reassign the slices to a new route and this route will be added to the solution

--- GOO/Model/Optimizers/Strategies/GeneticRouteStrategy.cs
+++ GOO/Model/Optimizers/Strategies/GeneticRouteStrategy.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using GOO.Utilities;
+
 namespace GOO.Model.Optimizers.Strategies
 {
     public class GeneticRouteStrategy : Strategy
     {
-        private Route firstRouteToModify;
+        private Route originalRoute;
+        private Route newAbominationRoute;
+        private Tuple<Days, int, List<Route>> planningForSelectedRoute;
         private Random random;
 
         public GeneticRouteStrategy()
@@ -39,23 +43,88 @@ namespace GOO.Model.Optimizers.Strategies
             if (targetCluster == null)
                 return null; // Just in case something weird happens
 
-            firstRouteToModify = targetCluster.Routes[firstRouteIndex];
+            originalRoute = targetCluster.Routes[firstRouteIndex];
+            double originalTravelTime = originalRoute.TravelTime;
+            int numOfSlices = random.Next(1, originalRoute.Orders.Count - 2);
 
+            List<int> sliceIndices = new List<int>();
+            for (int i = 0; i < numOfSlices; i++)
+            {
+                bool hasAddedAnIndex = false;
+                do
+                {
+                    int sliceIndex = random.Next(1, numOfSlices);
+                    if (!sliceIndices.Contains(sliceIndex))
+                    {
+                        sliceIndices.Add(sliceIndex);
+                        hasAddedAnIndex = true;
+                    }
+                } while (!hasAddedAnIndex);
+            }
+            sliceIndices.Sort(); // So that the indices are going for low to high
 
+            // COMMENCE THE GENETIC EXPERIMENTS! 
+            // OPERATION CUT & SPLICE AN ABOMINATION IS ACTIVE!
 
-            // if route is 1 long, don't cut just add
-            // remember the correctness of orderfrequency/daysrestriction
-            // Initial Cut & Splice whereby the new spliced childs are of an even length
-            // Uniform Crossover for #number of times where by you transfer 2-4 (random?) routes per iteration
-            // return new spliced abomination to test if it is better
+            List<Order>[] orderSlices = new List<Order>[sliceIndices.Count];
+            for (int i = 0; i < sliceIndices.Count; i++)
+            {
+                orderSlices[i] = new List<Order>();
+                int startIndex = i == 0 ? 0 : sliceIndices[i - 1];
+
+                for (int j = startIndex; j < sliceIndices[i]; j++)
+                    orderSlices[i].Add(originalRoute.Orders[j]);
+            }
+
+            // No that you've sliced up the orders in the selected route, it is time for randomizing the slices and putting it back together
+
+            newAbominationRoute = new Route(originalRoute.Day);
+            List<int> slicesIndexPutBack = new List<int>();
+            for (int i = 0; i < orderSlices.Length; i++)
+            {
+                bool hasPutBackASlice = false;
+                do
+                {
+                    int sliceIndex = random.Next(1, orderSlices.Length);
+                    if (!slicesIndexPutBack.Contains(sliceIndex))
+                    {
+                        for (int j = 0; j < orderSlices[sliceIndex].Count; j++)
+                            if (orderSlices[sliceIndex][j].OrderNumber != 0)
+                                newAbominationRoute.AddOrder(orderSlices[sliceIndex][j]);
+
+                        slicesIndexPutBack.Add(sliceIndex);
+                        hasPutBackASlice = true;
+                    }
+                } while (!hasPutBackASlice);
+            }
+
+            Console.WriteLine("Original Travel Time : {0}", originalTravelTime);
+            Console.WriteLine("Abomination Travel Time : {0}", newAbominationRoute.TravelTime);
+
+            planningForSelectedRoute = toStartFrom.getRoute(targetCluster.Routes);
+
+            if (planningForSelectedRoute.Item1 != originalRoute.Day)
+                Console.WriteLine("ERROR, found the wrong route!");
+
+            planningForSelectedRoute.Item3.Remove(originalRoute);
+            planningForSelectedRoute.Item3.Add(newAbominationRoute);
+
+            toStartFrom.RemoveItemFromPlanning(planningForSelectedRoute.Item1, planningForSelectedRoute.Item2);
+            toStartFrom.AddNewItemToPlanning(planningForSelectedRoute.Item1, planningForSelectedRoute.Item2, planningForSelectedRoute.Item3);
 
             return toStartFrom;
         }
 
         public override Solution undoStrategy(Solution toStartFrom)
         {
-            // revert abomination to patient zero
-            return null;
+            // Revert abomination to Patient Zero
+            planningForSelectedRoute.Item3.Remove(newAbominationRoute);
+            planningForSelectedRoute.Item3.Add(originalRoute);
+
+            toStartFrom.RemoveItemFromPlanning(planningForSelectedRoute.Item1, planningForSelectedRoute.Item2);
+            toStartFrom.AddNewItemToPlanning(planningForSelectedRoute.Item1, planningForSelectedRoute.Item2, planningForSelectedRoute.Item3);
+
+            return toStartFrom;
         }
     }
 }
