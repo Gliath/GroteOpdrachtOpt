ad2e888f9a6edbe3a90903fea0b40192f07a9453
Gliath <lukebuitendijk@gmail.com>
2015-12-23 05:24:48 +0100

    Changed constructor in ParentCluster to generate the initial DaysAvailable Added two methods, one to check if it is possible to add the specified day to a quadrant and one to assign a specific quadrant a certain day. The last method removes any DaysAvailable items that do not have the specified day. Also removes the the day specified from the list that do have the day specified (to prevent duplications)

--- GOO/Model/ParentCluster.cs
+++ GOO/Model/ParentCluster.cs
@@ -14,15 +14,59 @@ namespace GOO.Model
         public Point CentroidPoint { get; private set; }
         public override List<Order> OrdersInCluster { get; set; }
         public override Days DaysPlannedFor { get; set; } // available days
+        public List<Days> DaysAvailable { get; set; } // available days
 
         public override List<Route> Routes { get { return null; } set { return; } } // Not implemented in parent cluster
 
-        public ParentCluster(Point CentroidPoint, List<Order> OrdersInCluster, Days DaysPlannedFor, Cluster[] Quadrants)
+        public ParentCluster(Point CentroidPoint, List<Order> OrdersInCluster, Cluster[] Quadrants)
         {
             this.CentroidPoint = CentroidPoint;
             this.OrdersInCluster = OrdersInCluster;
-            this.DaysPlannedFor = DaysPlannedFor;
             this.Quadrants = Quadrants;
+
+
+            DaysAvailable = new List<Days>();
+
+            foreach (Order order in OrdersInCluster)
+                foreach (Days restriction in Data.Orders[order.OrderNumber].DayRestrictions)
+                    if (!DaysAvailable.Contains(restriction))
+                        DaysAvailable.Add(restriction);
+        }
+
+        public bool CanSetDaysPlanned(Days DayPlanned)
+        {
+            foreach (Days dayAvailable in DaysAvailable)
+                if (dayAvailable.HasFlag(DayPlanned))
+                    return true;
+
+            return false;
+        }
+
+        public void SetDaysPlannedForQuadrant(Days DayPlanned, int QuadrantNumber)
+        {
+            if (!CanSetDaysPlanned(DayPlanned))
+                return;
+
+            Quadrants[QuadrantNumber].DaysPlannedFor = DaysPlannedFor;
+
+            for (int i = 0; i < DaysAvailable.Count; i++)
+                if (!DaysAvailable[i].HasFlag(DayPlanned))
+                    DaysAvailable.Remove(DaysAvailable[i]);
+                else
+                    DaysAvailable[i] ^= DayPlanned; // Removes the day from availability
+
+            if (DaysAvailable.Count == 1)
+            {
+                int numOfUnassignedClusters = 0;
+                foreach (Cluster quadrant in Quadrants)
+                    if (quadrant.DaysPlannedFor == Days.None)
+                        numOfUnassignedClusters++;
+
+                if (numOfUnassignedClusters == 1)
+                    foreach (Cluster quadrant in Quadrants)
+                        if (quadrant.DaysPlannedFor == Days.None)
+                            quadrant.DaysPlannedFor = DaysAvailable[0]; // Only one item left so assign it
+            }
         }
 
         public override string ToString()
@@ -36,7 +80,7 @@ namespace GOO.Model
             builder.AppendLine(String.Format("Number of Orders: {0}", OrdersInCluster.Count));
             for (int i = 0; i < Quadrants.Length; i++)
                 builder.AppendLine(String.Format("Quadrant {0} has {1} orders", i, Quadrants[i].OrdersInCluster.Count));
-            
+
             builder.AppendLine(String.Format(""));
             return builder.ToString();
         }
